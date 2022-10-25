ca3400edbede5f761b3c12c6f350846e87bebee8
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2022-10-25 01:07:37 -0500

    Add props like parameters

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,11 @@
 
-
-const Card = (props) => {
-
+const Card = ({id, name, email}) => {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-			<img alt="robots" src={`https://robohash.org/${props.id}200x200`} />
+			<img alt="robots" src={`https://robohash.org/${id}200x200`} />
 			<div>
-				<h2>{props.name}</h2>
-				<p>{props.email}</p>
+				<h2>{name}</h2>
+				<p>{email}</p>
 			</div>
 		</div>
     );
