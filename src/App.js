e175272d1e0e58cfda9719b5cdbb6a5c9f4d1222
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2022-10-25 01:05:54 -0500

    Add Cardlist component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import Card from './Card';
 import { robots } from './robots';
+import CardList from './CardList';
 
 const App = () => {
 	return (
-		<div>
-			<h1>RoboFriends</h1>
-			<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-			<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-			<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-		</div>
+		<CardList robots={robots} />
 	);
 }
 
